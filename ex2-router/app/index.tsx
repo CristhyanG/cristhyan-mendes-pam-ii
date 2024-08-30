2aebfa267ca6b5a8cc14665cfc632cb2f46074eb
Davi Silva <davi-fernandosilva@hotmail.com>
2024-08-30 20:46:59 -0300

    Atualiza arquivo index e adiciona login em pasta dentro do app

--- ex2-router/app/index.tsx
+++ ex2-router/app/index.tsx
@@ -1,22 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 import  NavButton from '../components/NavButton'
 import {Link} from 'expo-router'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href={"/cadastro"}>
-      <NavButton label={"Cadastrar"}/>
-      </Link>
-     
+    <View style={styles.container} >
+      <View style={styles.barraPesquisa}> 
+      {/* barra de pesquisa*/}
+      </View>
+
+      <View style={styles.buttonLink}> 
+        <Link href={"login/cadastro"}>
+          <NavButton label={"Cadastrar"}/>
+        </Link>
+      </View>
+
     </View>
   );
-
  // const index = ({}) => é uma função resumida
 }
+
+const styles = StyleSheet.create 
+({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  barraPesquisa: {
+
+  },
+  buttonLink: {
+
+  },
+})
--- ex2-router/app/login/cadastro.tsx
+++ ex2-router/app/login/cadastro.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
-import  NavButton from '../components/NavButton'
+import  NavButton from '../../components/NavButton'
 import {Link} from 'expo-router'
 import { useState } from "react";
 
-export default function Index() {
+export default function cadastro() {
   return (
     //const imgLike = uri: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ficons%2Flike&psig=AOvVaw32NDZOfBcwBezu1fnVTvY5&ust=1725060942704000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMiXmPeum4gDFQAAAAAdAAAAABAT';
     <View
@@ -13,8 +13,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Link href={"/index"}>
-      <NavButton label={"Voltar"}/>
+      <Link href={"../"}>
+        <NavButton label={"Voltar"}/>
       </Link>
      
     </View>
