afcead3959c04f3dceb91dcd14e233e070a84473
Davi Silva <davi-fernandosilva@hotmail.com>
2024-08-30 22:36:03 -0300

    atualiza index e pagina de cadastro

---
+++ ex2-router/components/SearchBar.tsx
@@ -0,0 +1,10 @@
+
+import * as react from 'react';
+import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
+
+export default function SearchBar () { 
+    const [text, onChangeText] = React.useState('useless Text');
+    return (
+    //https://reactnative.dev/docs/textinput
+    );
+};
