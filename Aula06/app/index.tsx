4dc4adb7fad69c15729fa58e2541dbcb23eb14b1
CristhyanG <cristhyangiovannemg@gmail.com>
2024-10-07 22:31:53 -0300

    Inicia projeto com firestore database

---
+++ Aula06/app/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+// Configurações do Firebase (substitua pelos seus valores)
+const firebaseConfig = {
+  apiKey: "AIzaSyC3ZlXYt3nYnT-CgAZOSq628TkBfIozro8",
+  authDomain: "aula6-e9715.firebaseapp.com",
+  projectId: "aula6-e9715",
+  storageBucket: "aula6-e9715.appspot.com",
+  messagingSenderId: "236199966352",
+  appId: "1:236199966352:web:0838bc8569677be8d6ca15",
+  measurementId: "G-3P9721LWXL"
+};
+
+// Inicialize o Firebase antes de qualquer uso
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+
+  const sendData = async () => {
+    const nomesCollection = firebase.firestore().collection('Nomes');
+    try {
+      await nomesCollection.add({
+        Nome: nome,
+        Sobrenome: sobrenome
+      });
+      Alert.alert('Sucesso', 'Dados cadastrados com sucesso!');
+      setNome('');
+      setSobrenome('');
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Ocorreu um erro ao cadastrar os dados.');
+    }
+  };
+  
+  return (
+    <View>
+      <TextInput
+        placeholder="Nome"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        placeholder="Sobrenome"        
+        value={sobrenome}
+        onChangeText={setSobrenome}
+      />
+      <Button title="Cadastrar" onPress={sendData} />
+    </View>
+  );
+};
+
+export default App;
